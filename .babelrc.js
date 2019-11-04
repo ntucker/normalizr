cd469d673678863d65272f618c9dc09daecfc71c
Nathaniel Tucker <me@ntucker.me>
2019-11-04 07:23:25 -0800

    [internal] Build with anansi

--- .babelrc.js
+++ .babelrc.js
@@ -3,20 +3,5 @@ const { NODE_ENV, BABEL_ENV } = process.env;
 const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test';
 
 module.exports = {
-  presets: [['@babel/preset-env', { loose: true }]],
-  plugins: [
-    [
-      require('@babel/plugin-transform-runtime').default,
-      {
-        corejs: false,
-        helpers: true,
-        regenerator: true,
-        useESModules: !cjs,
-        version: '7.6.0'
-      }
-    ],
-    // cjs && 'transform-es2015-modules-commonjs',
-    '@babel/plugin-proposal-object-rest-spread',
-    ['@babel/plugin-proposal-class-properties', { loose: true }]
-  ].filter(Boolean)
+  presets: [['@anansi/babel-preset', { useESModules: !cjs, legacyDecorators: true }]]
 };
